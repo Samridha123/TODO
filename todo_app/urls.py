ccc4d729ed2005129ac9d2f8b5fd8c20f382f780
Samridha123 <149782993+Samridha123@users.noreply.github.com>
2024-02-01 17:25:45 +0530

    Add files via upload

---
+++ todo_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.add,name='add'),
+    #path('details/',views.details,name='details')
+    path('delete/<int:id>',views.delete,name='delete'),
+    path('update/<int:id>',views.update,name='update'),
+    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
+
+]
